7eb5f707e18ddc528c99350bcf3d10acbba717b9
pdizon020819 <pdizon020819@gmail.com>
2020-07-29 11:30:05 +0800

    Updates

---
+++ precache-manifest.8f2255cf2890107bc3575aadd31736f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b3c6dd796bbe06beef72b7aefbc83ff",
+    "url": "/scoreboard/index.html"
+  },
+  {
+    "revision": "432852645e1077988712",
+    "url": "/scoreboard/static/css/main.7cca5e0e.chunk.css"
+  },
+  {
+    "revision": "9d30d6910ae0630a8459",
+    "url": "/scoreboard/static/js/2.fca93150.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scoreboard/static/js/2.fca93150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "432852645e1077988712",
+    "url": "/scoreboard/static/js/main.2ec6fada.chunk.js"
+  },
+  {
+    "revision": "b33a85ed995fc3bd09d0",
+    "url": "/scoreboard/static/js/runtime-main.7d46900c.js"
+  }
+]);
